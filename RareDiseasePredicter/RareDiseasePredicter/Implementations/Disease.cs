c5de98038a3483f4645e0758d2697971addcf79b
Simon Spedsbjerg <simon@spedsbjerg.dk>
2023-03-23 10:30:29 +0100

    Create Disease.cs

---
+++ RareDiseasePredicter/RareDiseasePredicter/Implementations/Disease.cs
@@ -0,0 +1,37 @@
+﻿using RareDiseasePredicter.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RareDiseasePredicter.Implementations {
+    internal class Disease : Interfaces.IDisease {
+        List<ISymptom> symptoms = new List<ISymptom>();
+        string name = "";
+        public Disease(string name, List<ISymptom> symptoms) {
+            this.name = name;
+            this.symptoms = symptoms;
+        }
+
+        public Disease() {
+
+        }
+
+        public bool AddSymptoms(ISymptom symptom) {
+            symptoms.Add(symptom);
+            return true;
+        }
+
+        public string GetName() {
+            return name;
+        }
+
+        public ICollection<ISymptom> GetSymptoms() {
+            return symptoms;
+        }
+
+        public bool SetName(string name) {
+            this.name = name;
+            return true;
+        }
+    }
+}
