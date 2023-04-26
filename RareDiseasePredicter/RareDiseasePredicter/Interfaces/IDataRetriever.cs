a237db5f14439a2b79181a6908cb78198d147d2e
Simon Spedsbjerg <simon@spedsbjerg.dk>
2023-04-26 12:53:59 +0200

    Added interfaces

---
+++ RareDiseasePredicter/RareDiseasePredicter/Interfaces/IDataRetriever.cs
@@ -0,0 +1,10 @@
+﻿namespace RareDiseasePredicter.Interfaces {
+    //Reason for not making this static is if some dataretrievers have different permissions
+    interface IDataRetriever {
+        public Task<ICollection<IDisease>> GetDiseasesAsync();
+
+        public Task<ICollection<ISymptom>> GetSymptomsAsync();
+
+        public Task<ICollection<IRegion>> GetRegionsAsync();
+        }
+    }
