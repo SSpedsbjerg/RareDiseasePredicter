bf4997b5af2c219630fa79550e5617d5446ae7d8
Simon Spedsbjerg <71696995+SSpedsbjerg@users.noreply.github.com>
2023-05-03 07:15:09 +0200

    Fatal Error spottet in RDDeterminer, need version for showcase

--- RareDiseasePredicter/RareDiseasePredicter/Controller/DataController.cs
+++ RareDiseasePredicter/RareDiseasePredicter/Controller/DataController.cs
@@ -212,6 +212,187 @@ namespace RareDiseasePredicter.Controller {
             return jsonString;
             }
 
+        //TODO: Delete before production
+        [HttpGet]
+        [Route("/AddDummyData")]
+        public async Task<string> AddDummyData() {
+            string returnstring = "200 ";
+            List<IRegion> regions = new List<IRegion>();
+            Region region = new Region("head", -1);
+            regions.Add(region);
+            region = new Region("left_shoulder", -1);
+            regions.Add(region);
+            region = new Region("right_shoulder", -1);
+            regions.Add(region);
+            region = new Region("left_arm", -1);
+            regions.Add(region);
+            region = new Region("right_arm", -1);
+            regions.Add(region);
+            region = new Region("chest", -1);
+            regions.Add(region);
+            region = new Region("stomach", -1);
+            regions.Add(region);
+            region = new Region("left_leg", -1);
+            regions.Add(region);
+            region = new Region("right_leg", -1);
+            regions.Add(region);
+            region = new Region("left_hand", -1);
+            regions.Add(region);
+            region = new Region("right_hand", -1);
+            regions.Add(region);
+            region = new Region("left_foot", -1);
+            regions.Add(region);
+            region = new Region("right_foot", -1);
+            regions.Add(region);
+            foreach(IRegion region_ in regions) {
+                await DatabaseController.AddRegionAsync(region_);
+                returnstring += region_.Name + " ";
+            }
+
+            List<ISymptom> symptoms = new List<ISymptom>();
+            Symptom symptom = new Symptom("Tinnitus");//0
+            symptom.Description = "Ronstant ringing in the ear";
+            symptom.Regions.Add(regions[0]);
+            symptom = new Symptom("Dizziness");//1
+            symptom.Description = "null";
+            symptoms.Add(symptom);
+            symptom = new Symptom("Balance problems");//2
+            symptom.Description = "Having a hard time standing up without falling";
+            symptoms.Add(symptom);
+            symptom = new Symptom("Hearing loss");//3
+            symptom.Description = "null";
+            symptoms.Add(symptom);
+            symptom = new Symptom("Paralysis");//4
+            symptom.Description = "Unable to move in the region";
+            for(int i = 0; i < regions.Count; i++) {
+                symptom.Regions.Add((Region)regions[i]);
+                }
+            symptoms.Add(symptom);
+            symptom = new Symptom("Numbness");//5
+            symptom.Description = "null";
+            for(int i = 0; i < regions.Count; i++) {
+                symptom.Regions.Add((Region)regions[i]);
+                }
+            symptoms.Add(symptom);
+            symptom = new Symptom("Tingling");//6
+            symptom.Description = "null";
+            for(int i = 0; i < regions.Count; i++) {
+                symptom.Regions.Add((Region)regions[i]);
+                }
+            symptoms.Add(symptom);
+            symptom = new Symptom("Swallowing");//7
+            symptom.Description = "null";
+            symptom.Regions.Add(regions[0]);
+            symptoms.Add(symptom);
+            /*
+            symptom = new Symptom("Test");
+            symptom.Description = "Test";
+            symptom.Regions.Add(regions[0]);
+            symptoms.Add(symptom);
+            symptom = new Symptom("Test");
+            symptom.Description = "Test";
+            symptom.Regions.Add(regions[0]);
+            symptoms.Add(symptom);
+            symptom = new Symptom("Test");
+            symptom.Description = "Test";
+            symptom.Regions.Add(regions[0]);
+            symptoms.Add(symptom);
+            symptom = new Symptom("Test");
+            symptom.Description = "Test";
+            symptom.Regions.Add(regions[0]);
+            symptoms.Add(symptom);
+            symptom = new Symptom("Test");
+            symptom.Description = "Test";
+            symptom.Regions.Add(regions[0]);
+            symptoms.Add(symptom);
+            symptom = new Symptom("Test");
+            symptom.Description = "Test";
+            symptom.Regions.Add(regions[0]);
+            symptoms.Add(symptom);
+            symptom = new Symptom("Test");
+            symptom.Description = "Test";
+            symptom.Regions.Add(regions[0]);
+            symptoms.Add(symptom);
+            symptom = new Symptom("Test");
+            symptom.Description = "Test";
+            symptom.Regions.Add(regions[0]);
+            symptoms.Add(symptom);*/
+            foreach (ISymptom symptom_ in symptoms) {
+                await DatabaseController.AddSymptomAsync(symptom_);
+                returnstring += symptom_.Name + " ";
+                }
+
+            List<IDisease> diseases = new List<IDisease>();
+            Disease disease = new Disease();//0
+            disease.Description = "Acoustic neuromas are slow-growing tumors that can eventually cause a variety of symptoms by pressing against the eighth cranial nerve. Hearing loss in one ear (the ear affected by the tumor) is the initial symptom in approximately 90 percent of patients. Hearing loss is usually gradual, although in some rare cases it can be sudden. In some cases, hearing loss can also fluctuate (worsen and then improve). Hearing loss may be accompanied by ringing in the ears, a condition known as tinnitus, or by a feeling of fullness in the affected ear. In some cases, affected individuals may have difficulty understanding speech that is disproportional to the amount of hearing loss.";
+            disease.Href = "https://rarediseases.org/rare-diseases/acoustic-neuroma/#symptoms";
+            disease.Name = "Acoustic Neuroma";
+            for (int i = 0; i != 7; i++) {
+                disease.AddSymptoms(symptoms[i]);
+                }
+            diseases.Add(disease);
+            /*
+            disease = new Disease();
+            disease.Description = "Test";
+            disease.Href = "Test";
+            disease.Name = "Test";
+            disease.AddSymptoms(symptoms[0]);
+            diseases.Add(disease);
+            disease = new Disease();
+            disease.Description = "Test";
+            disease.Href = "Test";
+            disease.Name = "Test";
+            disease.AddSymptoms(symptoms[0]);
+            diseases.Add(disease);
+            disease = new Disease();
+            disease.Description = "Test";
+            disease.Href = "Test";
+            disease.Name = "Test";
+            disease.AddSymptoms(symptoms[0]);
+            diseases.Add(disease);
+            disease = new Disease();
+            disease.Description = "Test";
+            disease.Href = "Test";
+            disease.Name = "Test";
+            disease.AddSymptoms(symptoms[0]);
+            diseases.Add(disease);
+            disease = new Disease();
+            disease.Description = "Test";
+            disease.Href = "Test";
+            disease.Name = "Test";
+            disease.AddSymptoms(symptoms[0]);
+            diseases.Add(disease);
+            disease = new Disease();
+            disease.Description = "Test";
+            disease.Href = "Test";
+            disease.Name = "Test";
+            disease.AddSymptoms(symptoms[0]);
+            diseases.Add(disease);
+            disease = new Disease();
+            disease.Description = "Test";
+            disease.Href = "Test";
+            disease.Name = "Test";
+            disease.AddSymptoms(symptoms[0]);
+            diseases.Add(disease);
+            disease = new Disease();
+            disease.Description = "Test";
+            disease.Href = "Test";
+            disease.Name = "Test";
+            disease.AddSymptoms(symptoms[0]);
+            diseases.Add(disease);
+            disease = new Disease();
+            disease.Description = "Test";
+            disease.Href = "Test";
+            disease.Name = "Test";
+            disease.AddSymptoms(symptoms[0]);
+            diseases.Add(disease);*/
+            foreach (IDisease disease_ in diseases) {
+                await DatabaseController.AddDiseaseAsync(disease_);
+                returnstring += disease_.Name + " ";
+                }
+            return returnstring;
+            }
+
         //Gets a list of diseases
         [HttpGet]
         [Route("/Diseases")]
