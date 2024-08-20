bdc68af3f6ac6c4becee5718ac344bfafa646e0b
Rasmus Meldgaard <MrKetuz@users.noreply.github.com>
2024-08-20 10:39:59 +0200

    Remaking Frontend into Blazor

---
+++ RareDiseasePredictorFrontend/RareDiseasePredictor/Models/Disease.cs
@@ -0,0 +1,19 @@
+﻿namespace RareDiseasePredictor.Models
+{
+    public class Disease
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Href { get; set; }
+        public List<string> Symptoms { get; set; }
+
+        public Disease()
+        {
+            Symptoms = new List<string>();
+            Name = string.Empty;
+            Description = string.Empty;
+            Href = string.Empty;
+        }
+    }
+    
+}
---
+++ RareDiseasePredictorFrontend/RareDiseasePredictor/Models/Symptom.cs
@@ -0,0 +1,9 @@
+﻿namespace RareDiseasePredictor.Models
+{
+    public class Symptom
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public ICollection<Region> Regions { get; set; }
+    }
+}
