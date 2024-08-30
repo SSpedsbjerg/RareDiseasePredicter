9677040aefbbf13779678e450973c952a7f8a667
Rasmus Meldgaard <MrKetuz@users.noreply.github.com>
2024-08-30 02:51:03 +0200

    Updated objects and created Modals for Symptom
    Updated objects to accomdate for extra data, and created both the Add symptom and Edit symptom modals.
    
    Created the Update and Delete functionality aswell

--- RareDiseasePredictorFrontend/RareDiseasePredictor/Models/ApiDiseaseData.cs
+++ RareDiseasePredictorFrontend/RareDiseasePredictor/Models/ApiDiseaseData.cs
@@ -6,6 +6,7 @@ namespace RareDiseasePredictor.Models
     {
         public List<Disease> diseases { get; private set; }
         public List<Symptom> symptoms { get; private set; }
+        public List<Region> regions { get; private set; }
 
         public async Task LoadDiseaseData(HttpClient httpClient, string url)
         {
@@ -25,6 +26,17 @@ namespace RareDiseasePredictor.Models
             {
                 var jsonData = await response.Content.ReadAsStringAsync();
                 symptoms = JsonSerializer.Deserialize<List<Symptom>>(jsonData);
+            }
+
+        }
+
+        public async Task LoadRegionsData(HttpClient httpClient, string url)
+        {
+            var response = await httpClient.GetAsync(url);
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonData = await response.Content.ReadAsStringAsync();
+                regions = JsonSerializer.Deserialize<List<Region>>(jsonData);
             }
 
         }
