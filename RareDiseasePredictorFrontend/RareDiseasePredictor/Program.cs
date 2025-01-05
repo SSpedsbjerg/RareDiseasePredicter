4ea7e739edb4f7aab61c4c8b6c3711c827895ff5
Rasmus Meldgaard <MrKetuz@users.noreply.github.com>
2025-01-05 10:26:23 +0100

    Update Program.cs

--- RareDiseasePredictorFrontend/RareDiseasePredictor/Program.cs
+++ RareDiseasePredictorFrontend/RareDiseasePredictor/Program.cs
@@ -41,8 +41,6 @@ builder.Services.AddRazorComponents()
 builder.Services.AddSingleton<ApiDiseaseData>();
 builder.Services.AddSingleton<HoldTheToken>();
 
-
-
 var app = builder.Build();
 
 // Adds authentication functionality to app
@@ -56,10 +54,6 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
-
-
-
 
 app.UseHttpsRedirection();
 
